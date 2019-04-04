d6d639152a358cd2553f7b3c061a51b90782b1f0
Christian Pena <christianp@tarte.com>
2019-04-03 22:58:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Boilerplate React App
+## Getting started with the Webpack and Nodemon project builder
+
+### 1. Make sure to bundle files with webpack:
+    webpack -w
+
+### 1. Make sure to run the nodemon server locally:
+    nodemon server.js
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+/* using simple version of component since it is just presentational */
+var Navigation = () => {
+    return (
+        <div>
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Time App</li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/" activeClassName="active-link">Countdown</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <ul className="menu">
+                        <li className="menu-text">Created by <a href="http://www.christianpenaalba.com" target="_blank">Christian Pena</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+module.exports = Navigation;
