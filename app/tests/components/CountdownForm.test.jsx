4c05e7444da5220ae516099202890f4f5c96c957
Christian Pena <christianp@tarte.com>
2019-06-12 17:12:56 -0400

    added count down form component and wired it up to pass data to parent component. also added test files to test some of the components

---
+++ app/tests/components/CountdownForm.test.jsx
@@ -0,0 +1,28 @@
+var expect    = require('expect');
+var React     = require('react');
+var ReactDOM  = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $         = require('jQuery');
+
+/* Custom Components */
+var CountdownForm = require('CountdownForm');
+
+describe('CountdownForm', () => {
+    it('should exist', () => {
+        expect(CountdownForm).toBeTruthy();
+    });
+
+    it('should call onSetCountdown if valid seconds entered', () => {
+        // TODO: need to fix the spy, not working as expected
+        //var spy = expect.createSpy();
+        //var spy = jest.fn();
+        //spy();
+        var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+        var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+        countdownForm.refs.seconds.value = '109';
+        TestUtils.Simulate.submit($el.find('form')[0]);
+
+        expect(spy).toHaveBeenCalledWith(109);
+    });
+});
